f923122513da0e44f5068a1ed4f176eb1132c6c3
sovia0108 <sovialajuba0108@gmail.com>
2023-12-02 21:51:33 +0700

    fix part

--- dataekspedisi.php
+++
@@ -1,4 +0,0 @@
-<option value="">--Pilih Ekspedisi--</option>
-<option value="pos">POS Indonesia</option>
-<option value="tiki">TIKI</option>
-<option value="jne">JNE</option>
--- view/user/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-unset($_SESSION['user']);
-$_SESSION['success'] = "Thank You";
-header('Location: '.url());
-?>
